0e734d042c3543e92fcf5894383512f7ad524bdc
Dom Brassey <dombrassey@gmail.com>
2025-01-30 17:07:54 -0800

    troubleshoot 1 tile issue

--- make-map.py
+++ make-map.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import folium
+# from screeninfo import get_monitors
 
 # Load the CSV file
 df = pd.read_csv('data.csv')
 
+# m = get_monitors()
+# m_width = m[0].width
+# m_height = m[0].height
+
+# print(m_width)
+# print(m_height)
+
 # Create a map centered around the mean latitude and longitude
-m = folium.Map(location=[df['latitude'].mean(), df['longitude'].mean()], zoom_start=12)
+m = folium.Map(location=[df['latitude'].mean(), df['longitude'].mean()], zoom_start=12, width = '100%', height = '100%')
 
 # Add points to the map
 for _, row in df.iterrows():
